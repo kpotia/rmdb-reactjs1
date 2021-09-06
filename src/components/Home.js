927539a8dc3857887c95ca520b58e3b7e576b7d3
Mr Kpoti <patrickamtepe@gmail.com>
2021-09-06 21:16:20 +0000

    UPDATE: add spinner component

--- src/components/Home.js
+++ src/components/Home.js
@@ -8,6 +8,7 @@ import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL} from '../config';
 import HeroImage from './HeroImage';
 import Grid from './Grid';
 import Thumb from './Thumb';
+import Spinner from './Spinner';
 
 
 // Hooks
@@ -45,6 +46,7 @@ const Home = () => {
                 />
             ))}
         </Grid>
+        <Spinner />
         </>
       )
 }
