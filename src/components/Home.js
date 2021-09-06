f9af8bde3e6690bd9a1e5b7143108d809bf3a781
Mr Kpoti <patrickamtepe@gmail.com>
2021-09-06 20:03:06 +0000

    UPDATE: add heroimage to home page

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,7 +3,10 @@ import react from 'react';
 import API from '../API';
 // config
 import { POSTER_SIZE, BACKDROP_SIZE, IMAGE_BASE_URL} from '../config';
+
 // components 
+import HeroImage from './HeroImage';
+
 
 // Hooks
 import { useMovieFetch } from '../hooks/useMovieFetch'
@@ -16,10 +19,16 @@ const Home = () => {
 
     console.log(state);
     return (
-        <div>
-            Home Page
-        </div>
-    )
+        <>
+            {state.results[0] && 
+            <HeroImage 
+                image = {`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
+                title = {state.results[0].original_title}
+                text = {state.results[0].overview}
+            />
+            }
+        </>
+      )
 }
 
 export default Home;
