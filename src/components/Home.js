2d98eb8af0c64a49e38687675a8de47d3a7435c3
Mr Kpoti <patrickamtepe@gmail.com>
2021-09-07 00:17:34 +0000

    UPDATE: Add search to Home

--- src/components/Home.js
+++ src/components/Home.js
@@ -9,6 +9,7 @@ import HeroImage from './HeroImage';
 import Grid from './Grid';
 import Thumb from './Thumb';
 import Spinner from './Spinner';
+import SearchBar from './SearchBar';
 
 
 // Hooks
@@ -18,19 +19,20 @@ import NoImage from '../images/no_image.jpg';
 
 const Home = () => {
     
-    const {state, loading, error}  = useMovieFetch();
+    const {state, loading, error,searchTerm, setSearchTerm} = useMovieFetch();
 
     console.log(state);
     return (
         <>
-            {state.results[0] && 
+            { !searchTerm  && state.results[0] ?
             <HeroImage 
                 image = {`${IMAGE_BASE_URL}${BACKDROP_SIZE}${state.results[0].backdrop_path}`}
                 title = {state.results[0].original_title}
                 text = {state.results[0].overview}
-            />
+            /> : null
             }
-        <Grid header='Popular Movies'>
+        <SearchBar setSearchTerm={setSearchTerm} />
+        <Grid header={searchTerm ? 'Search Result':'Popular Movies'}>
             {state.results.map(movie => (
                 // <div key={movie.id}>{movie.title}</div>
                 <Thumb
