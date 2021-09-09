35ec390a1ec1383259af64ddb8f4baaaec508c7c
Mr Kpoti <patrickamtepe@gmail.com>
2021-09-09 16:43:48 +0000

    Create Load More Button

---
+++ src/components/Button/index.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+// styles 
+import { Wrapper } from './Button.style';
+
+ const Button = ({text, callback}) => {
+    return (
+        <Wrapper type="button" onClick={callback}>
+            {text}
+        </Wrapper>
+    )
+}
+
+export default Button;
