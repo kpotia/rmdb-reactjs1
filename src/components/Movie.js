b0994efb400c4976dfd3722a791cb20ff232ef42
Mr Kpoti <patrickamtepe@gmail.com>
2021-09-10 06:22:56 +0000

    ADD: Movie Component

---
+++ src/components/Movie.js
@@ -0,0 +1,30 @@
+import React from 'react';
+import { useParams } from 'react-router';
+// config
+import { IMAGE_BASE_URL, POSTER_SIZE } from '../config'
+
+// Components
+import Grid from './Grid';
+import Spinner from './Spinner';
+
+// Hooks 
+import { useMovieFetch } from '../hooks/useMovieFetch';
+// Image
+import NoImage from '../images/no_image.jpg'
+import BreadCrumb from './BreadCrumb';
+
+export const Movie = () => {
+    const {movieId}=useParams();
+
+    const { state: movie, loading, error } = useMovieFetch(movieId);
+    if(loading) return <Spinner />
+    if(error) return <div>Something went wrong</div>
+    console.log(movie);
+    return (
+        <> 
+            <BreadCrumb movieTitle={movie.original_title} />
+        </>
+    )
+}
+
+export default Movie;
