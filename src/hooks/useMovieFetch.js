8192ea0aefaa0c00b2cb3d300862a56edc059436
Mr Kpoti <patrickamtepe@gmail.com>
2021-09-10 06:23:22 +0000

    ADD: useMovieFetch Hook

--- src/hooks/useMovieFetch.js
+++ src/hooks/useMovieFetch.js
@@ -1,54 +1,41 @@
-import { useState, useEffect, useRef} from 'react';
-// API
+import { useState, useEffect } from "react";
+
 import API from '../API';
 
-const initialState = {
-    page:0,
-    results: [],
-    total_pages: 0,
-    total_results: 0,
-}
-
-export const useMovieFetch = () => {
-    const [searchTerm, setSearchTerm] = useState('');
-    const [state, setState] = useState(initialState);
-    const [loading, setLoading] = useState(false);
+export const useMovieFetch = movieId => {
+    const [state, setState] = useState({});
+    const [loading, setLoading] = useState(true);
     const [error, setError] = useState(false);
-    const [isLoadingMore, setIsLoadingMore] = useState(false);
 
-    console.log(searchTerm);
-    const fetchMovies = async (page, searchTerm = "") => {
-        try{
-            setError(false);
-            setLoading(true);
-
-            const movies = await API.fetchMovies(searchTerm,page);
-            // console.log(movies)
-
-            setState(prev => ({
-                ...movies,
-                results:
-                page > 1 ? [...prev.results, ...movies.results] : [...movies.results]
-            }))
-        }catch(error){
-            setError(true);
-        }
-        setLoading(false);
-    }
-
-    // initial and search
-    useEffect( () => {
-        setState(initialState)
-        fetchMovies(1, searchTerm)
-    }, [searchTerm]);
-
-    // load More
     useEffect(()=>{
-        if(!isLoadingMore) return;
+        const fetchMovie = async () => {
+            try {
+                setLoading(true);
+                setError(false);
 
-        fetchMovies(state.page+1, searchTerm);
-        setIsLoadingMore(false);
+                const movie = await API.fetchMovie(movieId);
+                const credits = await API.fetchCredits(movieId);
 
-    }, [isLoadingMore], searchTerm, state.page);
-    return {state, loading, error,searchTerm, setSearchTerm, setIsLoadingMore};
+                const directors = credits.crew.filter(
+                    member => member.job === 'Director'
+                );
+
+                setState({
+                    ...movie, 
+                    actors: credits.cast,
+                    directors
+                });
+
+                setLoading(false);
+            } catch (error) {
+                setError(true);
+            }
+        }
+
+        fetchMovie();
+    }, [movieId])
+
+
+
+    return {state, loading, error}
 }
